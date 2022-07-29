031516c7649dbca112b1042740bdaf5dfcf22f43
Brian Washington <bw49078@gmail.com>
2022-07-29 13:29:02 -0500

    added extra print that shows where words need to be replaced

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Madlib story unfinished
+print("It was a 'adjective', cold November day. I woke up to the 'adjective' smell of 'type of bird' roasting in the house_room downstairs. I 'past verb' down the stairs to see if I could help 'verb' the dinner. My mom said 'See if 'relative name' needs a fresh 'noun'.' So I carried a tray of glasses full of 'liquid' into the 'verb ending with ing' room. When I got there, I couldn't believe my 'plural body part'! There were 'plural noun' 'verb ending with ing' on the 'noun'")
+
 # variables
 adj = input("Enter adjective: ")
 adj2 = input("Enter another adjective: ")
@@ -14,5 +17,5 @@ ing_verb2 = input("Enter another verb ending in ing: ")
 plural_body_part = input("Enter a body part(plural): ")
 plural_noun = input("Enter a plural noun: ")
 
-# Madlib story
+# Madlib story finished
 print(f"It was a {adj}, cold November day. I woke up to the {adj2} smell of {bird} roasting in the {house_room} downstairs. I {past_verb} down the stairs to see if I could help {verb} the dinner. My mom said 'See if {relative_name} needs a fresh {noun}.' So I carried a tray of glasses full of {liquid} into the {ing_verb} room. When I got there, I couldn't believe my {plural_body_part}! There were {plural_noun} {ing_verb2} on the {noun2}")
