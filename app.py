c449820460acbee650a53f1a21584a239254dd97
Brian Washington <bw49078@gmail.com>
2022-07-29 13:15:26 -0500

    made game

---
+++ app.py
@@ -0,0 +1,18 @@
+# variables
+adj = input("Enter adjective: ")
+adj2 = input("Enter another adjective: ")
+bird = input("Enter a type of bird: ")
+house_room = input("Enter a room in a house: ")
+past_verb = input("Enter a past tense verb: ")
+verb = input("Enter a verb: ")
+relative_name = input("Enter a relative's name: ")
+noun = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+liquid = input("Enter a type of liquid: ")
+ing_verb = input("Enter a verb ending in ing: ")
+ing_verb2 = input("Enter another verb ending in ing: ")
+plural_body_part = input("Enter a body part(plural): ")
+plural_noun = input("Enter a plural noun: ")
+
+# Madlib story
+print(f"It was a {adj}, cold November day. I woke up to the {adj2} smell of {bird} roasting in the {house_room} downstairs. I {past_verb} down the stairs to see if I could help {verb} the dinner. My mom said 'See if {relative_name} needs a fresh {noun}.' So I carried a tray of glasses full of {liquid} into the {ing_verb} room. When I got there, I couldn't believe my {plural_body_part}! There were {plural_noun} {ing_verb2} on the {noun2}")
